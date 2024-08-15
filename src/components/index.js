2b27101223bf52430feab57993a5e1498739f211
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-08-15 23:09:14 +0530

    add login component

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,13 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-import Button from "./Button"
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import Login from "./Login";
+import Register from "./Register";
+import PostCard from "./PostCard";
 
-export { Header, Footer, Container, Logo , LogoutBtn, Button };
+export { Header, Footer, Container, Logo , LogoutBtn, Button,
+    Input, Select, Login, Register, PostCard
+ };
