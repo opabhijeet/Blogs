09612f8b3c0981dde1a1a05f651fe981e2a7c9d5
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-08-20 01:29:15 +0530

    fix issue of no redirection after signup

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -18,7 +18,7 @@ export class AuthService{
         try{
             const userAccount = await this.account.create(ID.unique(),email, password, name);
             if(userAccount){
-                this.login({email, password});
+                return await this.login({email, password});
             }
             else {
                 return userAccount;
