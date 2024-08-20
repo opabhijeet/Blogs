54fac60235e8a12c60878751dac00cb9882b58ba
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-08-21 02:44:56 +0530

    add api key for tinyMCE RTE

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from 'react-hook-form';
+import config from '../config/config';
 
 function RTE({name, control, label, defaultValue=""}) {
   return (
@@ -12,6 +13,7 @@ function RTE({name, control, label, defaultValue=""}) {
             control={control}
             render={({field: {onChange}})=>(
                 <Editor
+                    apiKey={config.tinyMCEApiKey}
                     initialValue={defaultValue}
                     init= {
                         {
